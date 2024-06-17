b76aadb98b4a9ced9c62f83cda8d2614c77b0a3a
masaapotat <masaakavulanya@gmail.com>
2024-06-17 16:03:03 +0300

    done w the the hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
+
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1><span>Hi, Masaa,</span> I'm a mobile developer, based in Mombasa, Kenya.</h1>
+      <p>
+        I specialize in creating innovative and user-friendly mobile
+        applications. With a strong background in both Android and iOS
+        development, I bring a passion for technology and a dedication to
+        delivering high-quality solutions.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My Resume</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
