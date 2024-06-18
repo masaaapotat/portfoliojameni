37a1f4ce7f84aa3a9012ad94ac497a43188fca24
masaapotat <masaakavulanya@gmail.com>
2024-06-18 09:27:46 +0300

    done with the work section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Services from './Components/Services/Services'
+import Work from './Components/MyWork/Work'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Services/>
+      <Work/>
 
 
     </div>
