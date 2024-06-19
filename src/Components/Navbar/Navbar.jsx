03e71def55c4b51466c1453aba51e5068b2c9e0c
masaapotat <masaakavulanya@gmail.com>
2024-06-19 08:56:44 +0300

    commiting changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.svg";
 import underline from "../../assets/nav_underline.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
+  const menuRef= useRef()
+ const openMenu = () => {
+   menuRef.current.style.right='0';
+ }
+ const closeMenu = () => {
+  menuRef.current.style.right='-350px';
+}
   return (
     <div className="navbar">
       <img src={logo} alt="logo" />
-      <ul className="nav-menu">
+      <img  src={menu_open} onClick={openMenu} alt="" className="toggle-icon" />
+      <ul ref={menuRef}  onClick={closeMenu} className="nav-menu">
+        {/* closes side nave */}
+        <img src={menu_close} alt="" className="navmob-close" />
         <li>
           <AnchorLink className="anchor-link" href="#home">
             <p onClick={() => setMenu("home")}>Home</p>
